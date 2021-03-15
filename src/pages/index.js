fdffbab6213a8b312dcf84004ce73d467add9ace
Jecyu <jecyu.lin@gmail.com>
2021-03-15 13:34:24 +0800

    fix(修复文章封面图不显示的问题): 📝2021-03-14 19:54:57

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { List, Icon, Tag, Skeleton } from 'antd';
 import Link from 'umi/link';
 import { connect } from 'dva';
 import moment from 'moment';
-// import Markdown from '@/components/markdown';
-import Archive from '@/components/archive'
+import Markdown from '@/components/markdown';
+import Archive from '@/components/archive';
 import styles from './index.less';
 
 const IconText = ({ type, text }) => (
@@ -25,19 +25,19 @@ class Index extends Component {
   componentDidMount() {
     // 判断是否是登录授权成功之后重定向的地址
     const { dispatch } = this.props;
-    const githubAuthCode = window.location.search.split('code=')[1]
+    const githubAuthCode = window.location.search.split('code=')[1];
     if (githubAuthCode) {
       dispatch({
         type: 'user/githubAuth',
         payload: {
-          githubAuthCode
-        }
+          githubAuthCode,
+        },
       }).then(result => {
         if (result) {
           localStorage.setItem('github_token', result);
           window.location.href = window.location.href.split(/[?#]/)[0];
         }
-      })
+      });
     }
 
     dispatch({
@@ -49,8 +49,8 @@ class Index extends Component {
     const issues = issuesList.map(item => {
       return {
         ...item,
-        time: moment(item.created_at).format('Y-M')
-      }
+        time: moment(item.created_at).format('Y-M'),
+      };
     });
 
     const result = issues.reduce((accumulator, current) => {
@@ -58,8 +58,7 @@ class Index extends Component {
         accumulator[current.time].total++;
         accumulator[current.time].item.push(current);
         accumulator[current.time].number = current.number;
-      }
-      else {
+      } else {
         accumulator[current.time] = {
           total: 1,
           item: [current],
@@ -69,19 +68,19 @@ class Index extends Component {
       return accumulator;
     }, {});
     return result;
-  }
+  };
 
   getTags = issuesList => {
     let issues = [];
     issuesList.forEach(element => {
-      issues = [...issues, ...element.labels]
+      issues = [...issues, ...element.labels];
     });
 
     issues.map(item => {
       return {
         ...item,
         id: `${item.id}`,
-      }
+      };
     });
 
     const result = issues.reduce((accumulator, current) => {
@@ -89,8 +88,7 @@ class Index extends Component {
         accumulator[current.id].total++;
         accumulator[current.id].color = current.color;
         accumulator[current.id].name = current.name;
-      }
-      else {
+      } else {
         accumulator[current.id] = {
           total: 1,
           color: current.color,
@@ -101,28 +99,27 @@ class Index extends Component {
     }, {});
 
     return result;
-  }
+  };
 
-  handleTagChange = (tag) => {
+  handleTagChange = tag => {
     // TODO
-  }
+  };
 
   render() {
     const {
-      home: {
-        issuesList = []
-      },
+      home: { issuesList = [] },
       issuesLoading = false,
     } = this.props;
 
     const timeLine = this.getTimeLine(issuesList);
     const tags = this.getTags(issuesList);
+    console.log('issuesList ->', issuesList);
 
     return (
       <div className={styles.root}>
         <Archive timeLine={timeLine} tags={tags} handleTagChange={this.handleTagChange} />
-        {
-          Array.isArray(issuesList) && <List
+        {Array.isArray(issuesList) && (
+          <List
             itemLayout="vertical"
             dataSource={issuesList}
             className={styles.list}
@@ -136,10 +133,12 @@ class Index extends Component {
                 className={styles.listItem}
                 key={item.title}
                 actions={[
-                  <Link to={`/${item.number}?scroll=like`}><IconText type="like-o" text={item.reactions['+1']} /></Link>
-                  ,
-                  <Link to={`/${item.number}?scroll=comment`}><IconText type="message" text={item.comments} /></Link>
-                  ,
+                  <Link to={`/${item.number}?scroll=like`}>
+                    <IconText type="like-o" text={item.reactions['+1']} />
+                  </Link>,
+                  <Link to={`/${item.number}?scroll=comment`}>
+                    <IconText type="message" text={item.comments} />
+                  </Link>,
                 ]}
                 extra={
                   <img
@@ -147,37 +146,52 @@ class Index extends Component {
                     style={{ borderRadius: 5 }}
                     alt="cover picture"
                     // src={item.body.match(/!\[.+?\]\((.+?[^)]*)\)/)[1]}
+                    src={(/!\[(.*?)\]\((.*?)\)/).exec(item.body) ? (/!\[(.*?)\]\((.*?)\)/).exec(item.body)[2]: "默认占位图"}
                   />
                 }
               >
                 <Skeleton loading={issuesLoading} active avatar>
                   <List.Item.Meta
                     title={
-                      <Link to={`/${item.number}`} className={styles.title}>{item.title}</Link>
+                      <Link to={`/${item.number}`} className={styles.title}>
+                        {item.title}
+                      </Link>
                     }
                   />
-                  {/* <Markdown dataSource={item.body.match(/<p>(.*?)<\/p>/)[0]} /> */}
+                  <Markdown
+                    dataSource={
+                      item.body.match(/<p>(.*?)<\/p>/) ? item.body.match(/<p>(.*?)<\/p>/)[0] : null
+                    }
+                  />
                   <div>
                     <div style={{ marginBottom: 10, display: 'flex', alignItems: 'center' }}>
-                      <Icon type="calendar" /><span style={{ marginRight: 10, marginLeft: 5 }}>{moment(item.created_at).format('Y-M-D')}</span>
-                      {item.labels && item.labels.length !== 0 && <Icon type="tag" style={{ marginRight: 5 }} />}
-                      {item.labels && item.labels.map(label => {
-                        return (
-                          <Tag
-                            key={label.id}
-                            style={{ background: `#${label.color}`, color: '#fff' }}
-                          >{label.name}</Tag>)
-                      }
+                      <Icon type="calendar" />
+                      <span style={{ marginRight: 10, marginLeft: 5 }}>
+                        {moment(item.created_at).format('Y-M-D')}
+                      </span>
+                      {item.labels && item.labels.length !== 0 && (
+                        <Icon type="tag" style={{ marginRight: 5 }} />
                       )}
+                      {item.labels &&
+                        item.labels.map(label => {
+                          return (
+                            <Tag
+                              key={label.id}
+                              style={{ background: `#${label.color}`, color: '#fff' }}
+                            >
+                              {label.name}
+                            </Tag>
+                          );
+                        })}
                     </div>
                   </div>
                 </Skeleton>
               </List.Item>
             )}
           />
-        }
+        )}
       </div>
-    )
+    );
   }
 }
 
